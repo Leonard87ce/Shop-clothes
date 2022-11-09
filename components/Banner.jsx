7036291bf90e1a831077a738e844bc8fda778000
leonard87ce <lcepeda99556@universidadean.edu.co>
2022-11-08 21:58:38 -0500

    Part one header and icons

---
+++ components/Banner.jsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export const Banner = () => {
+    return (
+        <div className="">
+
+        </div>
+    )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+        graphikMedium: ["Graphik Medium", "sans-serif"],
+        graphikLight: ["Graphik Light", "sans-serif"],
+        graphikBold: ["Graphik Bold", "sans-serif"],
+    },
+  },
+  plugins: [],
+}
